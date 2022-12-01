1a45791f89aaf991b858dd821d3fe24a0a2d3be5
sonakshi kumari <sonakshi.kumari24@gmail.com>
2022-12-02 02:10:05 +0530

    Time: 7 ms (62.56%), Space: 42.4 MB (50.76%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
@@ -3,19 +3,14 @@ class Solution {
         Character arr[] = {'a','e','i','o','u','A','E','I','O','U'};
         Set<Character> set = new HashSet<>(Arrays.asList(arr));
         int n = s.length();
+        int mid = n/2;
         int a = 0, b = 0;
-        for(int i=0;i<n/2;i++) {
+        for(int i=0;i<mid;i++) {
             char c = s.charAt(i);
-            if(set.contains(c)) {
-                a++;
-            }
+            char d = s.charAt(mid+i);
+            if(set.contains(c)) a++;
+            if(set.contains(d)) a--;
         }
-        for(int i=n/2;i<n;i++) {
-            char c = s.charAt(i);
-            if(set.contains(c)) {
-                b++;
-            }
-        }
-        return a==b;
+        return a==0;
     }
 }
