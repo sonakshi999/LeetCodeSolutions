953bf7a7650d9cb32433e19f96525d0fd1cb5fa8
sonakshi kumari <sonakshi.kumari24@gmail.com>
2022-12-21 19:20:27 +0530

    Time: 3 ms (56.74%), Space: 44.9 MB (27.63%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.java
+++ 0841-keys-and-rooms/0841-keys-and-rooms.java
@@ -1,12 +1,12 @@
 class Solution {
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
-        Stack<Integer> sc = new Stack<>();
+        Queue<Integer> sc = new LinkedList<>();
         sc.add(0);
         boolean seen[] = new boolean[rooms.size()];
         seen[0] = true;
         
         while(!sc.isEmpty()) {
-            int curr = sc.pop();
+            int curr = sc.poll();
             for(int i : rooms.get(curr)) {
                 if(seen[i] == false) {
                     seen[i] = true;
