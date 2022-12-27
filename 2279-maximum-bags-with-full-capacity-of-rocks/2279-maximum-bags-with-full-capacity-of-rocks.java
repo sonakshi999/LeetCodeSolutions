72dfc16ca52de9108e39d91c2aa811f0632a198d
sonakshi kumari <sonakshi.kumari24@gmail.com>
2022-12-27 15:53:04 +0530

    Time: 15 ms (94.69%), Space: 53.7 MB (78.47%) - LeetHub

--- 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.java
+++ 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.java
@@ -1,26 +1,20 @@
 class Solution {
     public int maximumBags(int[] capacity, int[] rocks, int additionalRocks) {
         int n = rocks.length;
-        int ans = 0;
-        ArrayList<Integer> diff = new ArrayList<>();
+        int diff[] = new int[n];
         for(int i=0;i<n;i++) {
-            if(rocks[i]==capacity[i]) {
-                ans++;
+            diff[i] = capacity[i]-rocks[i];
+        }
+        Arrays.sort(diff);
+        int step = 0;
+        for(int i : diff) {
+            if(additionalRocks >= i) {
+                step++;
+                additionalRocks -= i;
             } else {
-                diff.add(capacity[i]-rocks[i]);
+                break;
             }
         }
-        int arr[] = new int[diff.size()];
-        for(int i=0;i<diff.size();i++) {
-            arr[i] = diff.get(i);
-        }
-        Arrays.sort(arr);
-        for(int i=0;i<arr.length;i++) {
-            if(additionalRocks >= arr[i]) {
-                ans++;
-                additionalRocks -= arr[i];
-            }
-        }
-        return ans;
+        return step;
     }
 }
