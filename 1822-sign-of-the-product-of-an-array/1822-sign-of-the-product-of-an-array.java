c29172f39f4321921881c90ca88ce19f14b1776a
sonakshi kumari <sonakshi.kumari24@gmail.com>
2023-05-03 01:40:56 +0530

    Time: 0 ms (100.00%), Space: 42.3 MB (50.25%) - LeetHub

--- 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.java
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.java
@@ -4,13 +4,10 @@ class Solution {
         int countN = 0;
         for(int i=0;i<nums.length;i++) {
             if(nums[i] == 0) {
-                countZ++;
+                return 0;
             }else if(nums[i] < 0) {
                 countN++;
             }
-        }
-        if(countZ != 0) {
-            return 0;
         }
         if(countN % 2 != 0){
             return -1;
