0538f5f0ef19d8dc5065937d0226b5183fa28544
sonakshi kumari <sonakshi.kumari24@gmail.com>
2023-09-02 00:40:40 +0530

    Time: 11 ms (13.02%), Space: 46.2 MB (79.39%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] countBits(int n) {
+        int[] ans = new int[n+1];
+        for(int i=0;i<=n;i++) {
+            ans[i] = solve(i);
+        }
+        return ans;
+    }
+    int solve(int n) {
+        if(n==0) {
+            return 0;
+        }
+        if(n==1) {
+            return 1;
+        }
+        if(n%2==0) {
+            return solve(n/2);
+        }
+        if(n%2==1) {
+            return 1+solve(n/2);
+        }
+        return 1+solve(n/2);
+    }
+}
