09cdfbcd71593366afbc51577a83a613d5817f28
sonakshi kumari <sonakshi.kumari24@gmail.com>
2023-09-03 21:13:10 +0530

    Time: 0 ms (100.00%), Space: 38.9 MB (94.44%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     public int uniquePaths(int m, int n) {
         int dp[][] = new int[m][n];
         return countPath(0,0,m,n,dp);
@@ -13,4 +13,25 @@ class Solution {
         if(dp[i][j] != 0) { return dp[i][j]; }
         return dp[i][j] = countPath(i+1,j,m,n,dp) + countPath(i,j+1,m,n,dp);
     }
+}*/
+
+
+// TC = o(n*m);
+//SC = O(n*m);
+class Solution {
+    public int uniquePaths(int m, int n) {
+        // total path = m+n-2 (c) m-1 or m+n-2 (c) n-1
+        //int ans = (m+n-2) C (m-1)
+        int N = m+n-2;
+        int r = m-1;
+        double res = 1;
+        for(int i=1;i<=r;i++) {
+            res = res * (N-r+i)/i;
+        }
+        return (int) res;
+    }
 }
+
+
+//TC = O(m-1) or O(n-1)
+//SC = O(1)
