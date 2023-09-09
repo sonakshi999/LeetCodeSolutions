e18ede16f588614c3d43cbe1a941abfacdf24d15
sonakshi kumari <sonakshi.kumari24@gmail.com>
2023-09-10 00:33:50 +0530

    Time: 0 ms (100.00%), Space: 39 MB (78.28%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,37 +1,19 @@
-/*class Solution {
+class Solution {
     public int uniquePaths(int m, int n) {
-        int dp[][] = new int[m][n];
-        return countPath(0,0,m,n,dp);
+        int dp[][] = new int[m+1][n+1];
+        return solve(0,0,m,n,dp);
     }
-    int countPath(int i,int j,int m,int n,int[][] dp){
-        if(i>=m || j>=n) {
-            return 0;
+    int solve(int i,int j,int m, int n, int dp[][]) {
+        if(dp[i][j] != 0) {
+            return dp[i][j];
         }
         if(i==m-1 && j==n-1) {
             return 1;
         }
-        if(dp[i][j] != 0) { return dp[i][j]; }
-        return dp[i][j] = countPath(i+1,j,m,n,dp) + countPath(i,j+1,m,n,dp);
-    }
-}*/
-
-
-// TC = o(n*m);
-//SC = O(n*m);
-class Solution {
-    public int uniquePaths(int m, int n) {
-        // total path = m+n-2 (c) m-1 or m+n-2 (c) n-1
-        //int ans = (m+n-2) C (m-1)
-        int N = m+n-2;
-        int r = m-1;
-        double res = 1;
-        for(int i=1;i<=r;i++) {
-            res = res * (N-r+i)/i;
+        if(i >= m || j>= n) {
+            return 0;
         }
-        return (int) res;
+        
+        return dp[i][j] = solve(i,j+1,m,n,dp) + solve(i+1,j,m,n,dp);
     }
 }
-
-
-//TC = O(m-1) or O(n-1)
-//SC = O(1)
