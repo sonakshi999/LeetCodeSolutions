b2b84a31ab8df37c3f5d0726957312055cabea6a
sonakshi kumari <sonakshi.kumari24@gmail.com>
2023-10-02 15:00:10 +0530

    Time: 16 ms (52.37%), Space: 44.2 MB (56.20%) - LeetHub

--- 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.java
+++ 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.java
@@ -1,47 +1,15 @@
 class Solution {
     public boolean winnerOfGame(String colors) {
-        if(colors.length() <= 2) { 
-            return false;
-        }
-        int a = 0, b = 0;
-        for(int i=0;i<colors.length();i++) {
-            char c = colors.charAt(i);
-            int count = 1;
-            if(c == 'A') {
-                for(int j=i+1;j<colors.length();j++) {
-                    if(colors.charAt(j) == c) {
-                        count++;
-                        i++;
-                    }else{
-                        break;
-                    }
-                }
-                if(count>=3) {
-                    a = a + count-2;
+        int alice = 0, bob = 0;
+        for(int i=1;i<colors.length()-1;i++) {
+            if(colors.charAt(i-1) == colors.charAt(i) && colors.charAt(i) == colors.charAt(i+1)) {
+                if(colors.charAt(i) == 'A'){
+                    alice++;
+                }else{
+                    bob++;
                 }
             }
         }
-        for(int i=0;i<colors.length();i++) {
-            char c = colors.charAt(i);
-            int count = 1;
-            if(c == 'B') {
-                for(int j=i+1;j<colors.length();j++) {
-                    if(colors.charAt(j) == c) {
-                        count++;
-                        i++;
-                    }else{
-                        break;
-                    }
-                }
-                if(count>=3) {
-                    b = b + count-2;
-                }
-            }
-        }
-        System.out.println(a+" "+b);
-        if(a>b) {
-            return true;
-        }
-        return false;
+        return alice>bob;
     }
 }
