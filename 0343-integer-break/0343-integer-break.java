35bc95b469d0ea9e6717caea3ca363e2d660871f
sonakshi kumari <sonakshi.kumari24@gmail.com>
2023-10-07 15:47:24 +0530

    Time: 0 ms (100.00%), Space: 39.5 MB (21.11%) - LeetHub

--- 0343-integer-break/0343-integer-break.java
+++ 0343-integer-break/0343-integer-break.java
@@ -1,14 +1,26 @@
 class Solution {
+    int dp[];
     public int integerBreak(int n) {
-        if(n<=3) {
+        if(n <= 3) {
             return n-1;
         }
-        int ans = 1;
-        while(n > 4) {
-            ans = ans * 3;
-            n = n-3;
+        dp = new int[n+1];
+        return fn(n);
+    }
+    int fn(int n) {
+        if(n<=3) {
+            return n;
+            // as dividing them will make product samller only
         }
-        ans = ans * n;
-        return ans;
+        if(dp[n] != 0) {
+            return dp[n];
+        }
+        int product = 1;
+        // n ko chhote chhote parts m divide karo starting from 2 as 1 se start kene pr product will decrease only as 5 = 1+4 pro = 4
+        for(int i = 2;i<=n;i++) {
+            product = Math.max(product, i*fn(n-i));
+        }
+        dp[n] = product;
+        return product;
     }
 }
