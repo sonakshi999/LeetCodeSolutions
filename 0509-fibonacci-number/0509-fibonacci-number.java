a1b9987750f7e94406323ee88ccd20400edf21be
sonakshi kumari <sonakshi.kumari24@gmail.com>
2023-10-15 20:58:43 +0530

    Time: 0 ms (100.00%), Space: 39 MB (70.04%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -2,14 +2,16 @@ class Solution {
     public int fib(int n) {
         if(n==0) return 0;
         int[] dp = new int[n+1];
-        //dp[0]=0;dp[1]=1;
-        if(n<=1) {
+        Arrays.fill(dp,-1);
+        return fn(n,dp);
+    }
+    int fn(int n , int dp[]) {
+        if(n <= 1) {
             return n;
         }
-        if(dp[n] !=0) {
+        if(dp[n] != -1) {
             return dp[n];
         }
-        dp[n] = fib(n-1)+fib(n-2);
-        return dp[n];
+        return  dp[n] = fn(n-1,dp) + fn(n-2,dp);
     }
 }
