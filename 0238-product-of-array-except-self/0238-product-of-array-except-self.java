ae79a3f247f49c94bef21cfd387d9c27fd34f1e1
sonakshi kumari <sonakshi.kumari24@gmail.com>
2023-12-01 23:27:43 +0530

    Time: 2 ms (63.94%), Space: 51.4 MB (89.20%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -9,12 +9,13 @@ class Solution {
         right[nums.length-1] = 1;
         for(int i=nums.length-2;i>=0;i--) {
             right[i]=right[i+1]*nums[i+1];
+            left[i]=left[i]*right[i];
         }
-        int ans[] = new int[nums.length];
+        /*int ans[] = new int[nums.length];
         for(int i = 0;i<nums.length;i++) {
             ans[i] = left[i]*right[i];
-        }
-        return ans;
+        }*/
+        return left;
     }
 }
 
