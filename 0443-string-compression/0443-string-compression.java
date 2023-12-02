dcb2298f3eeba8e76d831318396f5d81dd2ec02b
sonakshi kumari <sonakshi.kumari24@gmail.com>
2023-12-02 23:34:11 +0530

    Time: 2 ms (30.36%), Space: 43.2 MB (44.87%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int compress(char[] chars) {
+        
+        int n = chars.length;
+        if(n<=1) {
+            return n;
+        }
+        List<Character> l = new ArrayList<>();
+        for(int i=0;i<n;i++) {
+            char curr = chars[i];
+            int j = i+1;
+            int count = 1;
+            while(j<n && chars[j]==curr) {
+                count++;
+                j++;
+            }
+            l.add(curr);
+            if(count > 1) {
+                String numberAsString = String.valueOf(count); 
+                char[] cha = numberAsString.toCharArray();
+                for(char c : cha) {
+                    l.add(c);
+                }
+            }
+            i = j-1;
+        }
+        int sz=l.size();
+        for(int i=0;i<sz;i++) {
+            chars[i] = l.get(i);
+        }
+        return sz;
+    }
+}
