2fad0e0ea7ff2b85778c439deb1e7f1c7e4be52a
sonakshi kumari <sonakshi.kumari24@gmail.com>
2023-12-04 23:55:59 +0530

    Time: 37 ms (24.45%), Space: 58.7 MB (6.05%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.java
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.java
@@ -1,20 +1,18 @@
 class Solution {
     public int maxOperations(int[] nums, int k) {
-        Arrays.sort(nums);
-        int start = 0;
-        int end = nums.length-1;
-        int ans = 0;
-        while(start < end) {
-            if(nums[start]+nums[end] == k) {
-                ans++;
-                start++;
-                end--;
-            }else if(nums[start]+nums[end]<k) {
-                start++;
+        HashMap<Integer,Integer>map=new HashMap<>();
+        int count=0;
+        for(int i=0;i<nums.length;i++){
+            //to check if that k-nums[i] present and had some value left or already paired
+            if(map.containsKey(k-nums[i])&&map.get(k-nums[i])>0){
+                count++;
+                map.put(k-nums[i],map.get(k-nums[i])-1);
             }else{
-                end--;
+                //getOrDefault is easy way it directly checks if value is 0 returns 0 where I added 1
+                //and if some value is present then it return that value "similar to map.get(i)" and I added 1 on it 
+                map.put(nums[i],map.getOrDefault(nums[i],0)+1);
             }
         }
-        return ans;
+        return count;
     }
 }
