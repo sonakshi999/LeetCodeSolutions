1ccf056ded1f44b0cd85c05058bcacb1460685c3
sonakshi kumari <sonakshi.kumari24@gmail.com>
2023-12-14 00:27:59 +0530

    Time: 1 ms (99.09%), Space: 43.6 MB (94.97%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -10,6 +10,7 @@ class Solution {
         right[n-1]=0;
         for(int i = n-2;i>=0;i--) {
             right[i] = right[i+1]+nums[i+1];
+        
         }
         for(int i=0;i<n;i++) {
             if(left[i]==right[i]){
