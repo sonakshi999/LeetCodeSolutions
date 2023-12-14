f9f41b14c71a4fda0cc78fd2c9007e397e468cd2
sonakshi kumari <sonakshi.kumari24@gmail.com>
2023-12-15 00:04:46 +0530

    Time: 2 ms (98.26%), Space: 41.1 MB (13.31%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
@@ -4,13 +4,6 @@ class Solution {
         for(int n : arr) {
             mp.put(n,mp.getOrDefault(n,0)+1);
         }
-        Set<Integer> ss = new HashSet<>();
-        for(Integer value : mp.values()) {
-            if(ss.contains(value)){
-                return false;
-            }
-            ss.add(value);
-        }
-        return true;
+        return mp.size() == new HashSet<>(mp.values()).size();
     }
 }
