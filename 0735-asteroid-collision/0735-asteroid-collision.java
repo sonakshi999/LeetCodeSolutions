2d91dff531c051d0dc9a8cec8e465c82aef84288
sonakshi kumari <sonakshi.kumari24@gmail.com>
2023-12-17 01:26:27 +0530

    Time: 6 ms (67.73%), Space: 43.8 MB (71.47%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.java
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -3,43 +3,46 @@ class Solution {
         Stack<Integer> sc = new Stack<>();
         for(int i=0;i<asteroids.length;i++) {
             int curr = asteroids[i];
-            
-            if(curr > 0) {
-                sc.add(curr);
-            } else {
-                int go = 0;
-                if(sc.isEmpty() || sc.peek()<0){
+            if(sc.isEmpty()){
+                sc.push(curr);
+            }else {
+                if(curr >= 0) {
                     sc.push(curr);
-                    
-                }else{
-                while(!sc.isEmpty() && sc.peek() > 0) {
-                    
-                    int temp = sc.pop();
-                    if(temp > Math.abs(curr)) {
-                        sc.push(temp);
-                        go=0;
-                        break;
-                    }else if(temp == Math.abs(curr)) {
-                        go =0;
-                        break;
+                }else {
+                    if(sc.peek() <= 0) {
+                        sc.push(curr);
                     }else{
-                        go =1;
+                        int go = 0;
+                        while(!sc.isEmpty()){
+                            if(sc.peek()>Math.abs(curr)){
+                                go=0;
+                                break;
+                            }else if(sc.peek()>0 && sc.peek()==Math.abs(curr)){ 
+                                go=0;
+                                sc.pop();
+                                break;
+                            }else if(sc.peek() < Math.abs(curr) && sc.peek() > 0){
+                               sc.pop();
+                                go=1;
+                            } else if(sc.peek()<0) {
+                                go=0;
+                                sc.push(curr);
+                                break;
+                            }
+                        }
+                        if(go==1){
+                            sc.push(curr);
+                        }
                     }
-                    
-                }
-                if(go==1){
-                    sc.push(curr);
                 }
             }
-            }
         }
-        
-        int sz = sc.size();
-        
-        int ans[] = new int[sz];
-        for(int i=sz-1;i>=0;i--) {
-            ans[i] = sc.pop();
+        int ans[] = new int[sc.size()];
+        int i = sc.size()-1;
+        while(!sc.isEmpty()){
+            ans[i--]=sc.pop();
         }
+        //Collections.reverse(Arrays.asList(ans));
         return ans;
     }
 }
