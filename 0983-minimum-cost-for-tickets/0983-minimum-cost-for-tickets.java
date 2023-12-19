19b2eef9f714e97110dded6735b71951a13cc5bc
sonakshi kumari <sonakshi.kumari24@gmail.com>
2023-12-19 14:51:11 +0530

    Time: 2 ms (41.27%), Space: 41.1 MB (22.11%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.java
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.java
@@ -1,30 +1,26 @@
 class Solution {
-    HashSet<Integer> isTravelNeeded = new HashSet<>();
+    Set<Integer> set = new HashSet<>();
     public int mincostTickets(int[] days, int[] costs) {
-        int lastDay = days[days.length - 1];
-        int dp[] = new int[lastDay + 1];
-        Arrays.fill(dp, -1);
-
-        // Mark the days on which we need to travel.
-        for (int day : days) {
-            isTravelNeeded.add(day);
+        int dp[] = new int[days[days.length-1]+1];
+        Arrays.fill(dp,-1);
+        for(int d:days) {
+            set.add(d);
         }
-       
-        return solve(days[0],days,costs,dp);
+        return solve(days[0],dp,days,costs);
     }
-    int solve(int currDay, int[] days , int[] costs, int[] dp) {
-        if (currDay > days[days.length - 1]) {
+    int solve(int currDay,int[] dp,int[] days,int[] costs) {
+        if(currDay > days[days.length-1]) {
             return 0;
         }
-        if(!isTravelNeeded.contains(currDay)){
-            return solve(currDay+1,days,costs,dp);
+        if(!set.contains(currDay)) {
+            return solve(currDay+1,dp,days,costs);
         }
         if(dp[currDay] != -1){
             return dp[currDay];
         }
-        int oneDay = costs[0] + solve(currDay+1,days,costs,dp);
-        int sevenDay = costs[1] + solve(currDay+7,days,costs,dp);
-        int thirtyDay = costs[2] + solve(currDay+30,days,costs,dp);
-        return dp[currDay] = Math.min(oneDay,Math.min(sevenDay,thirtyDay));
+        int c1 = costs[0]+solve(currDay+1,dp,days,costs);
+        int c2 = costs[1]+solve(currDay+7,dp,days,costs);
+        int c3 = costs[2]+solve(currDay+30,dp,days,costs);
+        return dp[currDay] = Math.min(c1,Math.min(c2,c3));
     }
 }
