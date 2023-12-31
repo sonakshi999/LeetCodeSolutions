cf4d77eca00f93bc99a4279c0a38f7787d2d8932
sonakshi kumari <sonakshi.kumari24@gmail.com>
2023-12-31 20:52:24 +0530

    Time: 4 ms (12.34%), Space: 41.8 MB (14.01%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.java
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.java
@@ -1,26 +1,26 @@
 class Solution {
-    Set<Integer> set = new HashSet<>();
+    Set<Integer> daysToInclude = new HashSet<>();
     public int mincostTickets(int[] days, int[] costs) {
         int dp[] = new int[days[days.length-1]+1];
         Arrays.fill(dp,-1);
-        for(int d:days) {
-            set.add(d);
+        for(int i:days) {
+            daysToInclude.add(i);
         }
-        return solve(days[0],dp,days,costs);
+        return solve(dp,days,costs,days[0]);
     }
-    int solve(int currDay,int[] dp,int[] days,int[] costs) {
+    int solve(int[] dp, int[] days, int[] costs, int currDay) {
         if(currDay > days[days.length-1]) {
             return 0;
         }
-        if(!set.contains(currDay)) {
-            return solve(currDay+1,dp,days,costs);
+        if(!daysToInclude.contains(currDay)) {
+            return solve(dp,days,costs,currDay+1);
         }
-        if(dp[currDay] != -1){
+        if(dp[currDay] != -1) {
             return dp[currDay];
         }
-        int c1 = costs[0]+solve(currDay+1,dp,days,costs);
-        int c2 = costs[1]+solve(currDay+7,dp,days,costs);
-        int c3 = costs[2]+solve(currDay+30,dp,days,costs);
-        return dp[currDay] = Math.min(c1,Math.min(c2,c3));
+        int day1=costs[0]+solve(dp,days,costs,currDay+1);
+        int day7=costs[1]+solve(dp,days,costs,currDay+7);
+        int day30=costs[2]+solve(dp,days,costs,currDay+30);
+        return dp[currDay] = Math.min(day1,Math.min(day7,day30));
     }
 }
