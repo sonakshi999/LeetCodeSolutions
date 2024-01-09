6c40cbd43addec89458d60775d0c4d39f2929242
sonakshi kumari <sonakshi.kumari24@gmail.com>
2024-01-09 23:23:33 +0530

    Time: 3 ms (18.15%), Space: 54.4 MB (13.35%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,26 +1,23 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int[] left = new int[nums.length];
-        left[0]=1;
-        for(int i=1;i<nums.length;i++) {
+        int n = nums.length;
+        int left[] = new int[n];
+        left[0] = 1;
+        for(int i=1;i<n;i++) {
             left[i] = left[i-1]*nums[i-1];
         }
-        int right[] = new int[nums.length];
-        right[nums.length-1] = 1;
-        for(int i=nums.length-2;i>=0;i--) {
-            right[i]=right[i+1]*nums[i+1];
+        int right[] = new int[n];
+        right[n-1] = 1;
+        for(int i = n-2;i>=0;i--) {
+            right[i] = right[i+1]*nums[i+1];
             left[i]=left[i]*right[i];
         }
-        /*int ans[] = new int[nums.length];
-        for(int i = 0;i<nums.length;i++) {
-            ans[i] = left[i]*right[i];
-        }*/
         return left;
     }
 }
 
 
-//original= [1,2,3,4]
-// left =   [1,1,2,6]
-// right =  [24,12,4,1]
-// ans =    [24,12,8,6]
+// 1,2,3,4
+// left = 1,1,2,6
+//right = 24,12,4,1
+// ans = 24,12,8,6
