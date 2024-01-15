55f268538ce146921be2557afa7bc87d5c23ab9d
sonakshi kumari <sonakshi.kumari24@gmail.com>
2024-01-16 02:03:09 +0530

    Time: 1 ms (98.84%), Space: 45.6 MB (22.57%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -2,15 +2,14 @@ class Solution {
     public int pivotIndex(int[] nums) {
         int n = nums.length;
         int left[] = new int[n];
-        left[0]=0;
-        for(int i =1;i<n;i++) {
+        int right[] = new int[n];
+        left[0] = 0;
+        for(int i=1;i<n;i++) {
             left[i] = nums[i-1]+left[i-1];
         }
-        int right[] = new int[n];
-        right[n-1]=0;
-        for(int i = n-2;i>=0;i--) {
-            right[i] = right[i+1]+nums[i+1];
-        
+        right[n-1] = 0;
+        for(int i=n-2;i>=0;i--) {
+            right[i]= nums[i+1]+right[i+1];
         }
         for(int i=0;i<n;i++) {
             if(left[i]==right[i]){
@@ -20,3 +19,8 @@ class Solution {
         return -1;
     }
 }
+
+
+// 1,7,3,6,5,6
+// left = 0,1,8,11,17,22
+//right = 27,20,17,11,6,0
