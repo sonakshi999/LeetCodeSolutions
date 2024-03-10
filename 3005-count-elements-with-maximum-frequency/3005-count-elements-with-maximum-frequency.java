286f354983e6196772902a1576d0189380dd4a48
sonakshi kumari <sonakshi.kumari24@gmail.com>
2024-03-10 14:45:30 +0530

    Time: 2 ms (69.60%), Space: 42.1 MB (55.63%) - LeetHub

--- 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.java
+++ 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.java
@@ -5,15 +5,13 @@ class Solution {
             mp.put(n,mp.getOrDefault(n,0)+1);
         }
         int max = 0;
+        int sum = 0;
         for(int n:mp.values()){
             if(n>max){
                 max = n;
-            }
-        }
-        int sum = 0;
-        for(Map.Entry<Integer,Integer> entry : mp.entrySet()){
-            if(entry.getValue()==max){
-                sum += entry.getValue();
+                sum = n;
+            }else if(n==max){
+                sum += n;
             }
         }
         return sum;
