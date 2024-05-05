1ae2cf510d6784262771ddd08da931cf60e2e8a6
sonakshi kumari <sonakshi.kumari24@gmail.com>
2024-05-05 20:37:13 +0530

    Time: 1 ms (11.55%), Space: 40.1 MB (81.49%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,10 +1,23 @@
 class Solution {
     public int uniquePaths(int m, int n) {
         int dp[][] = new int[m][n];
-        return find(m-1,n-1,dp);
+        for(int i=0;i<m;i++) {
+            for(int j=0;j<n;j++) {
+                if(i==0 && j==0) {
+                    dp[i][j]=1;
+                }else if(i==0) {
+                    dp[i][j] = dp[i][j-1];
+                }else if(j==0) {
+                    dp[i][j]= dp[i-1][j];
+                }else{
+                    dp[i][j]=dp[i-1][j]+dp[i][j-1];
+                }
+            }
+        }
+        return dp[m-1][n-1];
         
     }
-    int find(int i,int j, int[][]dp) {
+    /*int find(int i,int j, int[][]dp) {
         if(i<0 || j<0) {
             return 0;
         }
@@ -19,5 +32,5 @@ class Solution {
         int l = find(i-1,j,dp);
         int r = find(i,j-1,dp);
         return dp[i][j] = l+r;
-    }
+    }*/
 }
