52f42fee9d8a7468556b4fc29de1164a4e2b0b85
sonakshi kumari <sonakshi.kumari24@gmail.com>
2024-05-23 21:32:14 +0530

    Time: 4 ms (34.00%), Space: 45.1 MB (82.33%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -2,27 +2,25 @@ class Solution {
     public int minPathSum(int[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
+        int dp[][]= new int[n][m];
         
-        int dp[][] = new int[n][m];
-        for(int row[]:dp) {
-            Arrays.fill(row,-1);
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<m;j++) {
+                if(i==0 && j==0) {
+                    dp[i][j] = grid[i][j];
+                } else {
+                    int down = Integer.MAX_VALUE;
+                    int right = Integer.MAX_VALUE;
+                    if(i>0) {
+                        down = grid[i][j]+dp[i-1][j];
+                    }
+                    if(j>0) {
+                        right = grid[i][j]+dp[i][j-1];
+                    }
+                    dp[i][j] = Math.min(down,right);
+                }
+            }
         }
-        
-        return find(n-1,m-1,grid,dp);
-    }
-    
-    static int find(int i,int j,int[][] matrix,int[][] dp) {
-        if (i == 0 && j == 0)
-            return matrix[0][0];
-        if(i<0 || j<0) {
-            return 10000000;
-        }
-        if(dp[i][j] != -1) {
-            return dp[i][j];
-        }
-        
-        int up = matrix[i][j] + find(i-1,j,matrix,dp);
-        int left = matrix[i][j] + find(i,j-1,matrix,dp);
-        return dp[i][j] = Math.min(up,left);
+        return dp[n-1][m-1];
     }
 }
