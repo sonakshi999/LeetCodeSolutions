041efc539650b3aeb611c5c3042a438d634e929c
sonakshi kumari <sonakshi.kumari24@gmail.com>
2024-05-27 17:11:01 +0530

    Time: 67 ms (13.18%), Space: 45.2 MB (62.53%) - LeetHub

---
+++ 2336-smallest-number-in-infinite-set/2336-smallest-number-in-infinite-set.java
@@ -0,0 +1,25 @@
+class SmallestInfiniteSet {
+    PriorityQueue<Integer> pq = new PriorityQueue<>();
+    public SmallestInfiniteSet() {
+        for(int i=1;i<=1001;i++) {
+            pq.add(i);
+        }
+    }
+    
+    public int popSmallest() {
+        return pq.poll();
+    }
+    
+    public void addBack(int num) {
+        if(!pq.contains(num)) {
+            pq.add(num);
+        }
+    }
+}
+
+/**
+ * Your SmallestInfiniteSet object will be instantiated and called as such:
+ * SmallestInfiniteSet obj = new SmallestInfiniteSet();
+ * int param_1 = obj.popSmallest();
+ * obj.addBack(num);
+ */
